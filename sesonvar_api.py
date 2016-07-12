6001efe35c6dfe3df2bb1604f3195121c8900bf3
ipetrash <gil9red@gmail.com>
2016-07-12 13:16:26 +0500

    Рефакторинг

--- sesonvar_api.py
+++ sesonvar_api.py
@@ -12,6 +12,45 @@ from seasonvar_grabber import *
 from urllib.parse import quote_plus
 
 
+try:
+    import urllib.request as urllib  # python3
+except:
+    import urllib2 as urllib  # python2
+
+import json
+import logging
+
+
+class SeasonvarWebOpener:
+    __web_opener = None
+
+    @staticmethod
+    def __get_opener():
+        if SeasonvarWebOpener.__web_opener is None:
+            web_opener = urllib.build_opener()
+            web_opener.addheaders.append(('Cookie', 'sva=lVe324PqsI24'))
+            urllib.install_opener(web_opener)
+            SeasonvarWebOpener.__web_opener = web_opener
+
+        return SeasonvarWebOpener.__web_opener
+
+    @staticmethod
+    def get_json(url):
+        response = SeasonvarWebOpener.get_html(url)
+        return json.loads(response)
+
+    @staticmethod
+    def get_html(url):
+        try:
+            conn = SeasonvarWebOpener.__get_opener().open(url)
+            html = conn.read().decode('utf-8')
+            conn.close()
+            return html
+        except:
+            logging.exception('Error:')
+            return None
+
+
 class Serial:
     """Класс, описывающий сериал."""
 
