83726e012e090f04d3ef2ed6ca0b7849b6c1cc0d
ipetrash <gil9red@gmail.com>
2016-07-13 15:44:26 +0500

    Добавлен виджет-плеер с медийнами кнопками

--- gui.py
+++ gui.py
@@ -25,8 +25,131 @@ from PyQt5.QtMultimedia import *
 from sesonvar_api import Serial, SeasonvarApi
 
 
+class PlayerControls(QWidget):
+    """Класс описывает виджет с медийными кнопками."""
+
+    play_signal = pyqtSignal()
+    pause_signal = pyqtSignal()
+    stop_signal = pyqtSignal()
+    next_signal = pyqtSignal()
+    previous_signal = pyqtSignal()
+    changeVolume_signal = pyqtSignal(int)
+    changeMuting_signal = pyqtSignal(bool)
+    changeRate_signal = pyqtSignal(float)
+
+    def __init__(self):
+        super().__init__()
+        
+        self.playerState = QMediaPlayer.StoppedState
+        self.playerMuted = False
+
+        self.playButton = QToolButton()
+        self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+        self.playButton.clicked.connect(self.playClicked)
+    
+        self.stopButton = QToolButton()
+        self.stopButton.setIcon(self.style().standardIcon(QStyle.SP_MediaStop))
+        self.stopButton.setEnabled(False)
+        self.stopButton.clicked.connect(self.stop_signal)
+
+        self.nextButton = QToolButton()
+        self.nextButton.setIcon(self.style().standardIcon(QStyle.SP_MediaSkipForward))
+        self.nextButton.clicked.connect(self.next_signal)
+
+        self.previousButton = QToolButton()
+        self.previousButton.setIcon(self.style().standardIcon(QStyle.SP_MediaSkipBackward))
+        self.previousButton.clicked.connect(self.previous_signal)
+
+        self.muteButton = QToolButton()
+        self.muteButton.setIcon(self.style().standardIcon(QStyle.SP_MediaVolume))
+        self.muteButton.clicked.connect(self.muteClicked)
+    
+        self.volumeSlider = QSlider(Qt.Horizontal)
+        self.volumeSlider.setRange(0, 100)
+        self.volumeSlider.sliderMoved.connect(self.changeVolume_signal)
+
+        self.rateBox = QComboBox()
+        self.rateBox.addItem("0.5x", 0.5)
+        self.rateBox.addItem("1.0x", 1.0)
+        self.rateBox.addItem("2.0x", 2.0)
+        self.rateBox.setCurrentIndex(1)
+        self.rateBox.activated.connect(self.updateRate)
+    
+        layout = QHBoxLayout()
+        layout.setContentsMargins(0, 0, 0, 0)
+        layout.addWidget(self.stopButton)
+        layout.addWidget(self.previousButton)
+        layout.addWidget(self.playButton)
+        layout.addWidget(self.nextButton)
+        layout.addWidget(self.muteButton)
+        layout.addWidget(self.volumeSlider)
+        layout.addWidget(self.rateBox)
+        self.setLayout(layout)
+    
+    def state(self):
+        return self.playerState
+    
+    def setState(self, state):
+        if state != self.playerState:
+            self.playerState = state
+
+            if state == QMediaPlayer.StoppedState:
+                self.stopButton.setEnabled(False)
+                self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+
+            elif state == QMediaPlayer.PlayingState:
+                self.stopButton.setEnabled(True)
+                self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPause))
+
+            elif state == QMediaPlayer.PausedState:
+                self.stopButton.setEnabled(True)
+                self.playButton.setIcon(self.style().standardIcon(QStyle.SP_MediaPlay))
+    
+    def volume(self):
+        return self.volumeSlider.value()
+    
+    def setVolume(self, volume):
+        if self.volumeSlider:
+            self.volumeSlider.setValue(volume)
+    
+    def isMuted(self):
+        return self.playerMuted
+    
+    def setMuted(self, muted):
+        if muted != self.playerMuted:
+            self.playerMuted = muted
+
+            icon = QStyle.SP_MediaVolumeMuted if muted else QStyle.SP_MediaVolume
+            self.muteButton.setIcon(self.style().standardIcon(icon))
+    
+    def playClicked(self):
+        if self.playerState == QMediaPlayer.StoppedState or self.playerState == QMediaPlayer.PausedState:
+            self.play_signal.emit()
+
+        elif self.playerState == QMediaPlayer.PlayingState:
+            self.pause_signal.emit()
+    
+    def muteClicked(self):
+        self.changeMuting.emit(not self.playerMuted)
+    
+    def playbackRate(self):
+        return self.rateBox.itemData(self.rateBox.currentIndex()).toDouble()
+    
+    def setPlaybackRate(self, rate):
+        for i in range(self.rateBox.count()):
+            if qFuzzyCompare(rate, self.rateBox.itemData(i)):
+                self.rateBox.setCurrentIndex(i)
+                return
+    
+        self.rateBox.addItem("{}x".format(rate), rate)
+        self.rateBox.setCurrentIndex(self.rateBox.count() - 1)
+    
+    def updateRate(self):
+        self.changeRate.emit(self.playbackRate())
+
+
 # TODO:
-class SerialPlayer(QWidget):
+class SerialPlayerWindow(QMainWindow):
     def __init__(self, serial):
         super().__init__()
 
@@ -44,17 +167,58 @@ class SerialPlayer(QWidget):
         # Нужно задать какое-нибудь значение, потому что по умолчанию размер 0x0
         self.player.setVideoOutput(self.video_widget)
 
-        # TODO: лучше в dockwidget перенести
         # TODO: смена видео двойныс кликом или при нажатии на enter/return
         self.series_list = QListWidget()
         self.series_list.itemClicked.connect(lambda x: self.play(x.text()))
         for series in self.serial.list_of_series:
             self.series_list.addItem(series)
 
-        self.setLayout(QHBoxLayout())
-        self.layout().setContentsMargins(0, 0, 0, 0)
-        self.layout().addWidget(self.video_widget)
-        self.layout().addWidget(self.series_list)
+        self.controls = PlayerControls()
+        # controls->setState(player->state());
+        # controls->setVolume(player->volume());
+        # controls->setMuted(controls->isMuted());
+        #
+        # connect(controls, SIGNAL(play()), player, SLOT(play()));
+        # connect(controls, SIGNAL(pause()), player, SLOT(pause()));
+        # connect(controls, SIGNAL(stop()), player, SLOT(stop()));
+        # connect(controls, SIGNAL(next()), playlist, SLOT(next()));
+        # connect(controls, SIGNAL(previous()), this, SLOT(previousClicked()));
+        # connect(controls, SIGNAL(changeVolume(int)), player, SLOT(setVolume(int)));
+        # connect(controls, SIGNAL(changeMuting(bool)), player, SLOT(setMuted(bool)));
+        # connect(controls, SIGNAL(changeRate(qreal)), player, SLOT(setPlaybackRate(qreal)));
+        #
+        # connect(controls, SIGNAL(stop()), videoWidget, SLOT(update()));
+        #
+        # connect(player, SIGNAL(stateChanged(QMediaPlayer::State)),
+        #         controls, SLOT(setState(QMediaPlayer::State)));
+        # connect(player, SIGNAL(volumeChanged(int)), controls, SLOT(setVolume(int)));
+        # connect(player, SIGNAL(mutedChanged(bool)), controls, SLOT(setMuted(bool)));
+
+    # TODO:
+    #     if (!player->isAvailable()) {
+    #         QMessageBox::warning(this, tr("Service not available"),
+    #                              tr("The QMediaPlayer object does not have a valid service.\n"\
+    #                                 "Please check the media service plugins are installed."));
+    #
+    #         controls->setEnabled(false);
+    #         playlistView->setEnabled(false);
+    #         openButton->setEnabled(false);
+    # #ifndef PLAYER_NO_COLOROPTIONS
+    #         colorButton->setEnabled(false);
+    # #endif
+    #         fullScreenButton->setEnabled(false);
+    #     }
+
+        self.setCentralWidget(QWidget())
+        self.centralWidget().setLayout(QVBoxLayout())
+        self.centralWidget().layout().setContentsMargins(0, 0, 0, 0)
+        self.centralWidget().layout().addWidget(self.video_widget)
+        self.centralWidget().layout().addWidget(self.controls)
+
+        series_list_dock_widget = QDockWidget("Список серий")
+        series_list_dock_widget.setObjectName('series_list_dock_widget')
+        series_list_dock_widget.setWidget(self.series_list)
+        self.addDockWidget(Qt.RightDockWidgetArea, series_list_dock_widget)
 
         # self.video_widget.resize(400, 400)
         self.video_widget.setSizePolicy(QSizePolicy.Expanding, QSizePolicy.Expanding)
@@ -89,7 +253,7 @@ class SerialInfoWidget(QWidget):
         self._serial = None
 
         self._title = QLabel()
-        self._title .setWordWrap(True)
+        self._title.setWordWrap(True)
 
         self._cover = QLabel()
 
@@ -144,7 +308,6 @@ class SerialInfoWidget(QWidget):
             self._title.clear()
             self._cover.clear()
             self._description.clear()
-            self._series_list.clear()
 
             self._title.hide()
             self._cover.hide()
@@ -216,7 +379,7 @@ class MainWindow(QMainWindow):
         try:
             # Если окно-плеера сериала нет, добавляем, иначе показываем окно
             if serial not in self._serials_by_player_dict:
-                player = SerialPlayer(serial)
+                player = SerialPlayerWindow(serial)
                 self._serials_by_player_dict[serial] = player
             else:
                 player = self._serials_by_player_dict[serial]
@@ -251,5 +414,8 @@ if __name__ == '__main__':
     mw.show()
     # TODO: rem
     mw.serial_search.setText('gravity')
+
+    # spw = SerialPlayerWindow(None)
+    # spw.show()
 
     a.exec_()
